97c15e4b569ff56ff71f5fc35661496b1c5b6dd6
CarusoX <uziludu@gmail.com>
2019-05-23 12:26:04 -0300

    Estructuring app with modularized components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import Weather_API from './Weather_API'
+import Title from './Title'
+import Tabs from './Tabs'
+import SearchBar from './SearchBar'
 import './App.css'
+
 
 class App extends React.Component {
 
@@ -16,25 +18,32 @@ class App extends React.Component {
   render() {
     return(
 
-      <div className='BigDiv'>
+      <div>
+      <Title />
 
-        <h1 className='title'>MyWeather App</h1>
-      
-        <button className='buttons' onClick={() => this.setState({city: 'La Plata'})}>La Plata City!</button>
-        <button className='buttons' onClick={() => this.setState({city: 'Cordoba'})}>Cordoba City!</button>
-        <form>
-          <input className='textbox' type="text" placeholder="Search for a city here!"/>
-        </form>
-        
-        <Weather_API className='BigDiv'
-                     selected = {this.state.city}
-                     unit = {this.state.unit}/>
+      <SearchBar />
 
-        <button className='unitbutton' onClick={() => this.setState({unit: 'Kº'})}>Kelvin</button>
-        <button className='unitbutton' onClick={() => this.setState({unit: 'Cº'})}>Celcius</button>
-        <button className='unitbutton' onClick={() => this.setState({unit: 'Fº'})}>Fahrenheit</button>
-      
+      <Tabs/>
       </div>
+      // <div className='BigDiv'>
+
+      //   <h1 className='title'>MyWeather App</h1>
+      
+      //   <button className='buttons' onClick={() => this.setState({city: 'La Plata'})}>La Plata City!</button>
+      //   <button className='buttons' onClick={() => this.setState({city: 'Cordoba'})}>Cordoba City!</button>
+      //   <form>
+      //     <input className='textbox' type="text" placeholder="Search for a city here!"/>
+      //   </form>
+        
+      //   <Weather_API className='BigDiv'
+      //                selected = {this.state.city}
+      //                unit = {this.state.unit}/>
+
+      //   <button className='unitbutton' onClick={() => this.setState({unit: 'Kº'})}>Kelvin</button>
+      //   <button className='unitbutton' onClick={() => this.setState({unit: 'Cº'})}>Celcius</button>
+      //   <button className='unitbutton' onClick={() => this.setState({unit: 'Fº'})}>Fahrenheit</button>
+
+      // </div>
 
     );
   }
