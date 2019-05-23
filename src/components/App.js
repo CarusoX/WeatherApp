61700ee9f9c3d6dfceb90039e55eb8aa187d359b
Octaviotastico <octaviopercivaldi2@gmail.com>
2019-05-23 01:01:54 -0300

    Render function done in Weather_API class

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import Weather_API from './Weather_API'
 
 class App extends React.Component {
   render() {
     return(
       <div>
         <h1>Hi!</h1>
+        <p>Test with Buenos Aires city!</p>
       </div>
     );
   }
