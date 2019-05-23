c20536148c955307f8322bc5c2c4d33f2736081e
Octaviotastico <octaviopercivaldi2@gmail.com>
2019-05-23 02:11:04 -0300

    Added two buttons to change between Cordoba & La Plata weather - Everything working!

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,28 @@ import ReactDOM from 'react-dom'
 import Weather_API from './Weather_API'
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: ''
+    }
+  }
+
   render() {
     return(
+
       <div>
+
         <h1>MyWeather App</h1>
-        <p>Test with La Plata city!</p>
-        <Weather_API/>
-        <p>La cata se debe estar cagando de frio tho</p>
+      
+        <button onClick={() => this.setState({city: 'La Plata'})}>La Plata City!</button>
+        <button onClick={() => this.setState({city: 'Cordoba'})}>Cordoba City!</button>
+
+        <Weather_API selected = {this.state.city}/>
+      
       </div>
+
     );
   }
 }
--- src/components/Weather_API.js
+++ src/components/Weather_API.js
@@ -38,7 +38,7 @@ class Weather_API extends React.Component {
 
     // I'll make it work by name first, then by other options
     fetch_data() {
-        city_name = 'La Plata'
+        city_name = this.props.selected
         url = API_ENDPOINT + API_FETCH + '?q=' + city_name + '&appid=' + API_KEY_1;
         fetch(url).then((response) => {
             return response.json();
@@ -78,11 +78,10 @@ class Weather_API extends React.Component {
 
 
     render() {
-        if(this.state.working) {
+        if(this.state.working) { // We should change this, 'cos when it crashes once, then it never recovers
 
-            if(pingo === 1) {
+            if(this.props.selected !== '' && this.props.selected !== city_name) {
                 this.fetch_data();
-                pingo = 2;
             }
 
             return(
