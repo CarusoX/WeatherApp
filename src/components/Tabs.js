98fe85e822b20566befe815049b9e52a1d9dff4c
Lucas Caballero <lucaserfurt@hotmail.com>
2019-05-23 17:00:54 -0300

    Added component

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import WheaterCard from './WheaterCard.js'
+import ForecastCard from './ForecastCard.js'
 import { Tab } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 const panes = [
     { menuItem: 'Tab 1', render: () => <Tab.Pane><WheaterCard /> </Tab.Pane> },
-    { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+    { menuItem: 'Tab 2', render: () => <Tab.Pane><ForecastCard/></Tab.Pane> },
     { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
 ]
 
