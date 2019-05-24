15fa52c50bde4567ea63bf1658c7601abf92d7cd
CarusoX <uziludu@gmail.com>
2019-05-24 19:45:38 -0300

    Further improvements on PieChart

--- src/components/UVIIndex.js
+++ src/components/UVIIndex.js
@@ -1,51 +1,48 @@
-import React from 'react'
-import { Grid } from 'semantic-ui-react'
-import { PieChart, Cell, Pie, Sector } from 'recharts'
+import React, { PureComponent } from 'react'
+import { Divider, Container, Statistic, Grid } from 'semantic-ui-react'
+import { ResponsiveContainer, PieChart, Cell, Pie, Sector } from 'recharts'
 
 const data = [
   {
-    "name": "TrancaPalanca",
+    "name": "Low",
     "value": 60,
-    "description": "Se puede salir de joda hoy"
+    "description": "Wear sunglasses on bright hours!!",
+    "color": "green"
   },
   {
-    "name": "Group B",
-    "value": 60
-  },
-  {
-    "name": "Group C",
-    "value": 60
-  },
-  {
-    "name": "Group D",
-    "value": 60
-  },
-  {
-    "name": "Super re contra mega peligroso",
+    "name": "Moderate",
     "value": 60,
-    "description": "Mejor quedate en casa mirando Mr. Robot"
+    "description": "Stay in shade near midday when the sun is strongest.",
+    "color": "yellow"
+  },
+  {
+    "name": "High",
+    "value": 60,
+    "description": "Protection against skin and eye damage is needed.",
+    "color": "orange"
+  },
+  {
+    "name": "Very High",
+    "value": 60,
+    "description": "Take extra precautions because unprotected skin and eyes will be damaged and can burn quickly.",
+    "color": "brown"
+  },
+  {
+    "name": "Extreme",
+    "value": 60,
+    "description": "Take all precautions because unprotected skin and eyes can burn in minutes.",
+    "color": "purple"
   }
 ];
 
 const renderActiveShape = (props) => {
-  const RADIAN = Math.PI / 180;
   const {
-    cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
-    fill, payload, percent, value,
+    cx, cy, innerRadius, outerRadius, startAngle, endAngle,
+    fill, payload
   } = props;
-  const sin = Math.sin(-RADIAN * midAngle);
-  const cos = Math.cos(-RADIAN * midAngle);
-  const sx = cx + (outerRadius + 10) * cos;
-  const sy = cy + (outerRadius + 10) * sin;
-  const mx = cx + (outerRadius + 30) * cos;
-  const my = cy + (outerRadius + 30) * sin;
-  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-  const ey = my;
-  const textAnchor = cos >= 0 ? 'start' : 'end';
 
   return (
     <g>
-      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>{payload.name}</text>
       <Sector
         cx={cx}
         cy={cy}
@@ -64,59 +61,60 @@ const renderActiveShape = (props) => {
         outerRadius={outerRadius + 10}
         fill={fill}
       />
-      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`${payload.description}`}</text>
-      {/* <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-        {`(Rate ${(percent * 100).toFixed(2)}%)`}
-      </text> */}
+      <text x={cx} y={cy} dy={8} textAnchor="middle" style={{fontSize:20}} fill={fill}>{payload.name}</text>
     </g>
   );
 };
 
-const COLORS = ['green', 'yellow', 'orange', 'brown', 'purple'];
-
-export default class UVIIndex extends React.Component {
+export default class UVIIndex extends PureComponent {
 
   constructor(props) {
     super(props);
     this.state = {
-      activeIndex: 4
+      activeIndex: 2
     }
   }
 
   render() {
     return (
-      <Grid stretched centered>
-        <Grid.Row>
-          Current UVI index
-        </Grid.Row>
-        <Grid.Row>
-          9.1
-        </Grid.Row>
-        <Grid.Row>
-          <PieChart width={800} height={350}>
-            <Pie
-              data={data}
-              cx={400}
-              cy={175}
-              activeIndex={this.state.activeIndex}
-              activeShape={renderActiveShape}
-              innerRadius={60}
-              outerRadius={80}
-              endAngle={300}
-              fill="#8884d8"
-              paddingAngle={5}
-              dataKey="value"
-            >
-              {
-                data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-              }
-            </Pie>
-          </PieChart>
-        </Grid.Row>
-
-      </Grid>
+      <Container fluid>
+        <Grid textAlign='center' verticalAlign='middle' container columns='equal' relaxed stretched divided>
+          <Grid.Row>
+            <Grid.Column>
+              <Statistic>
+                <Statistic.Value>5,8</Statistic.Value>
+                <Statistic.Label>Current UVI index</Statistic.Label>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column style={{ width: '40%', height: 300 }}>
+              <ResponsiveContainer>
+                <PieChart>
+                  <Pie
+                    data={data}
+                    activeIndex={this.state.activeIndex}
+                    activeShape={renderActiveShape}
+                    innerRadius={70}
+                    outerRadius={100}
+                    endAngle={300}
+                    fill="#8884d8"
+                    paddingAngle={5}
+                    dataKey="value"
+                  >
+                    {
+                      data.map((entry, index) => <Cell key={`cell-${index}`} fill={entry.color} />)
+                    }
+                  </Pie>
+                </PieChart>
+              </ResponsiveContainer>
+            </Grid.Column>
+            <Grid.Column>
+              <Statistic>
+                <Statistic.Label>{data[this.state.activeIndex].description}</Statistic.Label>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     )
   }
 }
