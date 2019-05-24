0445980ce6c0b804c17c1fc534c294386ee36d38
Octaviotastico <octaviopercivaldi2@gmail.com>
2019-05-24 03:24:19 -0300

    Some changes on SearchBar - We need to reestructure in order to pass states easily!

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -4,9 +4,9 @@ import { Tab } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 const panes = [
-    { menuItem: 'Tab 1', render: () => <Tab.Pane attached={false}><WheaterCard /> </Tab.Pane> },
-    { menuItem: 'Tab 2', render: () => <Tab.Pane attached={false}>Tab 2 Content</Tab.Pane> },
-    { menuItem: 'Tab 3', render: () => <Tab.Pane attached={false}>Tab 3 Content</Tab.Pane> },
+    { menuItem: 'Current Weather', render: () => <Tab.Pane attached={false}><WheaterCard /> </Tab.Pane> },
+    { menuItem: 'Week Forecast', render: () => <Tab.Pane attached={false}>Tab 2 Content</Tab.Pane> },
+    { menuItem: 'UV Rays', render: () => <Tab.Pane attached={false}>Tab 3 Content</Tab.Pane> },
 ]
 
 class Tabs extends React.Component {
