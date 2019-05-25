eb89eb20883fffd46fcaf2be7175908ac5daa040
Lucas Caballero <lucaserfurt@hotmail.com>
2019-05-25 00:41:50 -0300

    Minor changes

--- src/components/ForecastCard.js
+++ src/components/ForecastCard.js
@@ -3,10 +3,6 @@ import { Segment, Grid, Icon } from 'semantic-ui-react'
 
 
 class ForecastCard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   state = {
       day: ['Sunday', 'Monday', 'Tuesday', 'Tuesday', 'Thursday', 'Friday', 'Saturday'],
       maxT: 0,
@@ -27,32 +23,31 @@ class ForecastCard extends React.Component {
             <Segment>{this.state.maxT}</Segment>
             <Segment>{this.state.minT}</Segment>
           </Grid.Column>
-        
     );
   }
 
-  set_day(maT, miT, d){
-      this.state.maxT= maT
-      this.state.minT = miT
+  set_day(k){
+      this.state.maxT= this.props.list[0].main.temp_max
+      this.state.minT = this.props.list[0].main.temp_min
+      this.state.date = ['', '', '']
+
+      for (var i = 2; i < 4; i++) {
+        this.state.date[0] += this.props.list[k].dt_txt[i]
+      }
+      for (var i = 5; i < 7; i++) {
+        this.state.date[1] += this.props.list[k].dt_txt[i]
+      }
+      for (var i = 8; i < 10; i++) {
+        this.state.date[2] += this.props.list[k].dt_txt[i]
+      }
+
       let key_month = [0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5]
       let day = this.state.date[2] - '0'
       let month = this.state.date[1] - '0' - 1
       let year = Math.floor((this.state.date[0] - '0')/4)
       let correction = key_month[month]
-      this.state.currentDay = (day + month + year + correction + 6) % 7
-  }
 
-  set_date(k){
-    this.state.date = ['', '', '']
-    for (var i = 2; i < 4; i++) {
-      this.state.date[0] += this.props.list[k].dt_txt[i]
-    }
-    for (var i = 5; i < 7; i++) {
-      this.state.date[1] += this.props.list[k].dt_txt[i]
-    }
-    for (var i = 8; i < 10; i++) {
-      this.state.date[2] += this.props.list[k].dt_txt[i]
-    }
+      this.state.currentDay = (day + month + year + correction + 6) % 7
   }
 
 
@@ -61,15 +56,10 @@ class ForecastCard extends React.Component {
       return (
         <Grid columns={5} divided>
           <Grid.Row stretched>
-            {this.set_day(0, 0, 0)}
             {this.day()}
-            {this.set_day(0, 0, 0)}
             {this.day()}
-            {this.set_day(0, 0, 0)}
             {this.day()}
-            {this.set_day(0, 0, 0)}
             {this.day()}
-            {this.set_day(0, 0, 0)}
             {this.day()}
           </Grid.Row>
         </Grid>
@@ -79,20 +69,15 @@ class ForecastCard extends React.Component {
       return (
         <Grid columns={5} divided>
           <Grid.Row stretched>
-                {this.set_date(0)}
-                {this.set_day(this.props.list[0].main.temp_max, this.props.list[0].main.temp_min, 0)}
+                {this.set_day(0)}
                 {this.day()}
-                {this.set_date(7)}
-                {this.set_day(this.props.list[7].main.temp_max, this.props.list[7].main.temp_min, 1)}
+                {this.set_day(7)}
                 {this.day()}
-                {this.set_date(15)}
-                {this.set_day(this.props.list[15].main.temp_max, this.props.list[15].main.temp_min, 2)}
+                {this.set_day(15)}
                 {this.day()}
-                {this.set_date(23)}
-                {this.set_day(this.props.list[23].main.temp_max, this.props.list[23].main.temp_min, 3)}
+                {this.set_day(23)}
                 {this.day()}
-                {this.set_date(31)}
-                {this.set_day(this.props.list[31].main.temp_max, this.props.list[31].main.temp_min, 4)}
+                {this.set_day(31)}
                 {this.day()}
           </Grid.Row>
         </Grid>
