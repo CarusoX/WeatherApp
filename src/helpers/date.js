2e72c520fff8d47f66a3c212c7fc8c98d3f7f770
CarusoX <uziludu@gmail.com>
2019-05-27 14:05:48 -0300

    Looking for month period

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,5 +1,9 @@
 export const getDateName = (dateStr) => {
     console.log(dateStr)
     const date = new Date(dateStr);
-    return date.toLocaleDateString('en-EN', { weekday: 'short' });        
+    return date.toLocaleDateString('en-EN', { weekday: 'short' });
+}
+
+export const getMonthPeriod = () => {
+    
 }
