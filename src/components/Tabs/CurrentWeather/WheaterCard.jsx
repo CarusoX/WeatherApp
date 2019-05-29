559076fcf28f75689020e192de4c0648bafd5e39
Octaviotastico <octaviopercivaldi2@gmail.com>
2019-05-28 22:47:15 -0300

    Added Icons

--- src/components/Tabs/CurrentWeather/WheaterCard.jsx
+++ src/components/Tabs/CurrentWeather/WheaterCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Segment, Grid, Icon, Divider } from 'semantic-ui-react'
+import { Segment, Grid, Image, Divider } from 'semantic-ui-react'
 import { getTemp } from '../../../helpers/index.ts'
 
-class WheaterCard extends React.Component {
+const path1 = '../../../icons/Theme1/'
+const path2 = '../../../icons/Theme2/'
 
+class WheaterCard extends React.Component {
+  
   render() {
     const {
       weather_id, weather_state, weather_description, weather_icon,
@@ -17,20 +20,23 @@ class WheaterCard extends React.Component {
           <Grid.Column>
 
             <Segment>
-              <Icon.Group size='massive'>
-                <Icon name='sun' color='yellow' />
-              </Icon.Group>
+            <Image src={require('../../../icons/Theme2/036-sun.png')}
+                   size='medium' verticalAlign='middle' />
             </Segment>
 
             <Segment>
               Max temp
               <Divider />
+              <Image src={require('../../../icons/Theme2/018-high temperature.png')}
+                     size='mini' verticalAlign='middle' />
               {getTemp(max_temp, unit)}
             </Segment>
 
             <Segment>
               Min temp
               <Divider />
+              <Image src={require('../../../icons/Theme2/022-low temperature.png')}
+                     size='mini' verticalAlign='middle' />
               {getTemp(min_temp, unit)}
             </Segment>
 
@@ -39,25 +45,33 @@ class WheaterCard extends React.Component {
 
             <Segment>
               Temp
-            <Divider />
+              <Divider />
+              <Image src={require('../../../icons/Theme2/039-thermometer.png')}
+                     size='mini' verticalAlign='middle' />
               {getTemp(temp, unit)}
             </Segment>
 
             <Segment>
               State
               <Divider />
+              <Image src={require('../../../icons/Theme2/045-weather.png')}
+                     size='mini' verticalAlign='middle' />
               {weather_state}
             </Segment>
 
             <Segment>
               Humidity
               <Divider />
+              <Image src={require('../../../icons/Theme2/019-humidity.png')}
+                     size='mini' verticalAlign='middle' />
               {humidity} %
             </Segment>
 
             <Segment>
               Wind
               <Divider />
+              <Image src={require('../../../icons/Theme2/048-wind.png')}
+                     size='mini' verticalAlign='middle' />
               {wind_speed} M/s
             </Segment>
 
@@ -67,18 +81,24 @@ class WheaterCard extends React.Component {
             <Segment>
               Sunrise
               <Divider />
+              <Image src={require('../../../icons/Theme2/012-dawn.png')}
+                     size='mini' verticalAlign='middle' />
               No se jaja salu2 AM
             </Segment>
 
             <Segment>
               Sunset
               <Divider />
+              <Image src={require('../../../icons/Theme2/037-sunset.png')}
+                     size='mini' verticalAlign='middle' />
               No se jaja salu2 PM
             </Segment>
 
             <Segment>
               Preassure
               <Divider />
+              <Image src={require('../../../icons/Theme2/026-pressure.png')}
+                     size='mini' verticalAlign='middle' />
               {pressure} hPa
             </Segment>
 
