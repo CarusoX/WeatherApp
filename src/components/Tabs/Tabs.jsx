02bc8cc9ec6ae14fae9605e3adf477f87eacc66b
Octaviotastico <octaviopercivaldi2@gmail.com>
2019-05-30 08:54:49 -0300

    Added third party segment to App

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -17,14 +17,6 @@ const errorPanes = (mode) => [
   { menuItem: 'UV Rays', render: () => <ErrorTab mode={mode} /> },
 ]
 
-/*
-
-Icons taken of:
-<div>Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" 		    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 		    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-<div>Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" 		    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 		    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-
-*/
-
 class Tabs extends React.Component {
   
   render() {
