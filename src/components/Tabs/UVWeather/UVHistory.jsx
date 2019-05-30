a24dd4993a13361e95374a0a2e4160f819bddd2e
CarusoX <uziludu@gmail.com>
2019-05-29 23:56:57 -0300

    Working on areaChart

--- src/components/Tabs/UVWeather/UVHistory.jsx
+++ src/components/Tabs/UVWeather/UVHistory.jsx
@@ -10,26 +10,22 @@ const getColor = (index) => {
   else if (index < 11) return 'red';
   else return 'purple';
 }
-const CustomizedDot = (props) => {
-  const {
-    cx, cy, value,
-  } = props;
-  return <Dot cx={cx} cy={cy} r={5} fill={getColor(value)} />
+
+const gradientOffset = (data) => {
+  const dataMax = Math.max(...data.map(i => i.value));
+  const dataMin = Math.min(...data.map(i => i.value));
+
+  if (dataMax <= 6) {
+    return 0;
+  }
+  if (dataMin >= 4) {
+    return 1;
+  }
+
+  return dataMax / (dataMax - dataMin);
 };
 
-class CustomizedAxisTick extends PureComponent {
-  render() {
-    const {
-      x, y, payload,
-    } = this.props;
-
-    return (
-      <g transform={`translate(${x},${y})`}>
-        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
-      </g>
-    );
-  }
-}
+const off = (data) => gradientOffset(data);
 
 export default class UVHistory extends PureComponent {
   static jsfiddleUrl = 'https://jsfiddle.net/alidingling/xqjtetw0/';
@@ -41,7 +37,7 @@ export default class UVHistory extends PureComponent {
           <AreaChart
             width={500}
             height={400}
-            data={data}
+            data={this.props.uv_history.map((x) => ({ 'date': x.date_iso.slice(5, 10).split('-').join('/'), 'uv': x.value }))}
             margin={{
               top: 10, right: 30, left: 0, bottom: 0,
             }}
@@ -52,8 +48,8 @@ export default class UVHistory extends PureComponent {
             <Tooltip />
             <defs>
               <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
-                <stop offset={off} stopColor="green" stopOpacity={1} />
-                <stop offset={off} stopColor="red" stopOpacity={1} />
+                <stop offset={off(this.props.uv_history)} stopColor="green" stopOpacity={1} />
+                <stop offset={off(this.props.uv_history)} stopColor="red" stopOpacity={1} />
               </linearGradient>
             </defs>
             <Area type="monotone" dataKey="uv" stroke="#000" fill="url(#splitColor)" />
